cb906430ace105234bb8c314f98a61410f9164d7
Matthew Erbs <matthew.erbs@merbla.com>
2015-03-29 14:59:15 +1000

    ideas

---
+++ Ideas/Playground/Playground/Tutorial.fs
@@ -0,0 +1,13 @@
+﻿open FSharp.Data
+open System.Net
+open System.IO
+[<Literal>]
+let sample = "https://s3-ap-southeast-2.amazonaws.com/cloudformation-templates-ap-southeast-2/Windows_Roles_And_Features.template"
+let apiUrl = "https://s3-ap-southeast-2.amazonaws.com/cloudformation-templates-ap-southeast-2/Windows_Roles_And_Features.template"
+
+
+type windowsTemplate = JsonProvider<sample>
+
+let myTemplate = windowsTemplate.Load(apiUrl)
+
+myTemplate.Resources.
